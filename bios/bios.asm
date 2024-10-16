03c05449d8b40b89ca8b23ced1d09ca9c5431bae
Linus Lind <linus.he.lind@gmail.com>
2024-10-16 14:56:00 +0200

    Start of bios

---
+++ bios/bios.asm
@@ -0,0 +1,36 @@
+        
+        include io.asm
+
+        org $f800 *Jumptable*
+init:   jmp init_
+ppinit: jmp init22
+serinit:jmp init681
+
+init_:   orcc #$50
+        lds #$0100
+        jsr init681
+        jsr init22
+        rts
+init681:lda #$0a
+        sta CRA_681
+        sta CRB_681
+        clra
+        sta IMR_681
+        sta OPCR_681
+        lda #$80
+        sta ACR_681
+        lda #$53
+        sta MRnA_681
+        sta MRnB_681
+        lda #$07
+        sta MRnA_681
+        sta MRnB_681
+        rts
+init22: clra
+        sta DDRA_22
+        sta DDRB_22
+        sta ACR_22
+        sta PCR_22
+        lda #$7f
+        sta IER_22
+        rts
---
+++ bios/io.asm
@@ -0,0 +1,39 @@
+        
+        * 65c22 at $f780*
+ORB_22  = $f780
+ORA_22  = $f781
+DDRA_22 = $f782
+DDRB_22 = $f783
+ACR_22  = $f78b
+PCR_22  = $f78c
+IFR_22  = $f78d
+IER_22  = $f78e
+
+        *68c681 at $f7c0*
+        *Port A registers*
+MRnA_681 = $f7c0
+SRA_681 = $f7c1
+CSRA_681 = $f7c1
+CRA_681 = $f7c2
+RHRA_681 = $f7c3
+THRA_681 = $f7c3
+        *Interupt, timer, and control registers*
+ACR_681 = $f7c4
+MISR_681 = $f7c2
+ISR_681 = $f7c5
+IMR_681 = $f7c5
+CTU_681 = $f7c6
+CTL_681 = $f7c7
+        *Port B registers*
+MRnB_681 = $f7c8
+SRB_681 = $f7c9
+CSRB_681 = $f7c9
+CRB_681 = $f7ca
+RHRB_681 = $f7cb
+THRB_681 = $f7cb
+        *IP & OP data and control registers*
+IPCR_681 = $f7c4
+IP_681  = $f7cd
+OPCD_681 = $f7cd
+SOP_681 = $f7ce
+COP_681 = $f7cf
